728cd23e88f741c1388f5cd8dd817ff657cfd386
seanj06 <seanj06@hotmail.com>
2023-02-20 22:43:21 +0000

    Add test model and create superuser

--- swim_class/admin.py
+++ swim_class/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from .models import Person
 # Register your models here.
--- swim_class/models.py
+++ swim_class/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Person(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.title
