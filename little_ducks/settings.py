f4b9f0b23f7c87c31b99b03a8500720397ba3d64
seanj06 <seanj06@hotmail.com>
2023-02-20 22:23:27 +0000

    Install and setup allauth

--- little_ducks/settings.py
+++ little_ducks/settings.py
@@ -46,7 +46,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'swim_class',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
