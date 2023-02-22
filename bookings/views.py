85a52da961682f8f6bfb8baedebf5436078d7e14
seanj06 <seanj06@hotmail.com>
2023-02-22 14:28:21 +0000

    Add skeleton form to booking template

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Booking
 
 
 # Create your views here.
 def booking(request):
-    return render(request, 'booking.html')
+    
+    booking_info = Booking.objects.all()
+
+    context = {
+        'booking_info': booking_info
+    }
+
+    return render(request, 'booking.html', context)
+
+
