fe9b1c4155e790f61ad35149939fbed57159b93d
seanj06 <seanj06@hotmail.com>
2023-02-22 13:29:29 +0000

    Add Booking Model

---
+++ bookings/book.py
@@ -0,0 +1,15 @@
+Type_of_class = (
+    ("Beginner", "Beginner"),
+    ("Intermediate", "Intermediate"),
+    ("Advanced", "Advanced"),
+)
+
+Time_Choices = (
+    ("9.00am", "9.00am"),
+    ("9.30am", "9.30am"),
+    ("10.00am", "10.00am"),
+    ("10.30am", "10.30am"),
+    ("11.00am", "11.00am"),
+    ("11.30am", "11.30am"),
+    ("12.00am", "12.00am"),
+)
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from .book import Type_of_class, Time_Choices
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    level = models.CharField(max_length=50, choices=Type_of_class, default="Intermediate")
+    date = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=Time_Choices, default="11.00am")
+    time_of_booking = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} date: {self.date} time: {self.time}"
